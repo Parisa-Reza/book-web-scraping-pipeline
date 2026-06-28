c352387592534c1d27a1ee0b3cefd5a435acceff
Parisa Reza <rezaparisa5@gmail.com>
2026-06-28 21:36:48 +0600

    added : settings confifuration

--- books_web_scraping/settings.py
+++ books_web_scraping/settings.py
@@ -1,11 +1,9 @@
-# Scrapy settings for books_web_scraping project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import os
+from pathlib import Path
+
+BASE_DIR = Path(os.environ.get("SCRAPY_OUTPUT_BASE", Path.cwd())).resolve()
+OUTPUT_DIR = BASE_DIR / "output"
+DATA_DIR = BASE_DIR / "data"
 
 BOT_NAME = "books_web_scraping"
 
@@ -14,74 +12,27 @@ NEWSPIDER_MODULE = "books_web_scraping.spiders"
 
 ADDONS = {}
 
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "books_web_scraping (+http://www.yourdomain.com)"
-
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Concurrency and throttling settings
-#CONCURRENT_REQUESTS = 16
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
-DOWNLOAD_DELAY = 1
+CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+DOWNLOAD_DELAY = 0.25  # 
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "books_web_scraping.middlewares.BooksWebScrapingSpiderMiddleware": 543,
-#}
+ITEM_PIPELINES = {
+    "books_web_scraping.pipelines.BooksWebScrapingPipeline": 300,
+}
 
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "books_web_scraping.middlewares.BooksWebScrapingDownloaderMiddleware": 543,
-#}
+FEEDS = {
+    str(OUTPUT_DIR / "books.json"): {"format": "json", "overwrite": True},
+    str(OUTPUT_DIR / "books.csv"): {"format": "csv", "overwrite": True},
+    str(OUTPUT_DIR / "books.xml"): {"format": "xml", "overwrite": True},
+}
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
+SQLITE_DB_PATH = str(DATA_DIR / "scraped_books_vault.db")
 
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "books_web_scraping.pipelines.BooksWebScrapingPipeline": 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = "httpcache"
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
-
-# Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
